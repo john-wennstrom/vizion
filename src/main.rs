02f30802816c5e7eabb9da346ee4553858694d98
johnw <johnw@coretrek.no>
2020-02-03 13:53:27 +0100

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 extern crate skew;
-extern crate image;
 
 use std::env;
 use skew::unskew;
@@ -12,22 +11,5 @@ fn main() {
     let src = env::args().nth(1).unwrap();
     let dst = env::args().nth(2).unwrap();
 
-    /*let src = Path::new(&src);
-    let dst = Path::new(&dst);
-
-    if !dst.is_dir() {
-        fs::create_dir(dst).expect("Failed to create output directory")
-    }*/
-
-    /*if !src.is_file() {
-        panic!("Input file does not exist");
-    }*/
-
     let _skew_lines = unskew(&src, &dst);
-
-    /*for (i, line) in skew_lines.iter().enumerate() {
-        if i % 10 == 0 {
-            println!("{:?}, {:?}", line.angle_in_degrees, line.r);
-        }
-    }*/
 }
