5799b3f56ef72b16de63e57590f5643b98d32868
johnny-human <john.wennstrom@protonmail.com>
2020-02-20 06:39:35 +0100

    Add gray image

--- libs/convolution/src/lib.rs
+++ libs/convolution/src/lib.rs
@@ -19,7 +19,7 @@ pub struct Img {
   width: u32,
   height: u32,
   raw: Vec<u8>,
-  pixels: Vec<Vec<[u8; 3]>>
+  pixels: Vec<Vec<u8>>
 }
 
 pub struct Runner {
@@ -68,10 +68,10 @@ impl Conv2d {
 
 impl<'s> Img {
   pub fn new(src: & str) -> Img {
-    let img = image::open(&src).expect("File not found").to_rgb();
+    let img = image::open(&src).expect("File not found").to_luma();
     let (width, height) = img.dimensions();
     let raw = img.into_raw().clone();
-    let pixels = Img::get_pixels(width, height, &raw);
+    let pixels = Img::get_pixels_gray(width, height, &raw);
 
     Img {
       width: width,
@@ -92,6 +92,41 @@ impl<'s> Img {
 
       // Add R/G/B value to pixel
       pixel[counter_pix] = i;
+
+      // If pixel is full, append it to image (using c2 as row index) and  set
+      // c1 as 0 to overwrite pixel RGB values
+      if counter_pix == 2 {
+        pixels[counter_row].push(pixel);
+        counter_pix = 0;
+      } else {
+
+        // Increase c1
+        counter_pix += 1;
+      }
+
+      // If the row has reached the end, and more rows can be pushed, increase c2 (and
+      // push a new row) otherwise break
+      if pixels[counter_row].len() as u32 == width {
+        if (pixels.len() as u32) < height {
+          pixels.push(vec![]);
+          counter_row += 1;
+        } else {
+          break;
+        }
+      }
+    }
+
+    println!("{:?}", pixels);
+
+    pixels
+  }
+
+  pub fn get_pixels_gray(width: u32, height: u32, raw: Vec<u8>) -> Vec<Vec<u8>> {
+    let mut pixels: Vec<Vec<u8>> = vec![];
+
+    for i in 0..height {
+      let mut row = &raw[i as usize..width as usize];
+
 
       // If pixel is full, append it to image (using c2 as row index) and  set
       // c1 as 0 to overwrite pixel RGB values
